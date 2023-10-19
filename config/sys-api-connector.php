4a940bb95cdc21dc51f0da67d26cd47c4d6da131
Ravish Patel <ravish@sysotel.ai>
2023-10-19 18:24:26 +0530

    update

--- config/sys-api-connector.php
+++ config/sys-api-connector.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'open_api' => [
+    'cms_open_api' => [
         'base_url' => env('SYSOTEL_CMS_OPEN_API_URL'),
         'client_id' => env('SYSOTEL_CMS_OPEN_API_CLIENT_ID'),
         'api_token' => env('SYSOTEL_CMS_OPEN_API_TOKEN'),
