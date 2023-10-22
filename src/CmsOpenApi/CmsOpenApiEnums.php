3bbc704734e3b47a3b4e86c6ed2e4b0aa56a6619
Ravish Patel <ravish@sysotel.ai>
2023-10-22 15:09:43 +0530

    update

--- src/CmsOpenApi/CmsOpenApiEnums.php
+++ src/CmsOpenApi/CmsOpenApiEnums.php
@@ -6,6 +6,8 @@ enum CmsOpenApiEnums
 {
 
     const PROPERTY_STATUS_ACTIVE = 'ACTIVE';
+    const PROPERTY_STATUS_INACTIVE = 'INACTIVE';
+    const PROPERTY_STATUS_BLOCKED = 'BLOCKED';
 
     const BOOKING_TYPE_DAILY = 'DAILY';
     const BOOKING_TYPE_HOURLY = 'HOURLY';
@@ -28,14 +30,110 @@ enum CmsOpenApiEnums
     const PROPERTY_TYPE_OTHER = 'OTHER';
 
     const SPACE_STATUS_ACTIVE = 'ACTIVE';
+    const SPACE_STATUS_INACTIVE = 'INACTIVE';
 
     const INVENTORY_ACCURACY_DAILY = 'DAILY';
     const INVENTORY_ACCURACY_HOURLY = 'HOURLY';
 
     const PRODUCT_STATUS_ACTIVE = 'ACTIVE';
+    const PRODUCT_STATUS_INACTIVE = 'INACTIVE';
 
     const MEAL_PLAN_CODE_EP = 'EP';
     const MEAL_PLAN_CODE_CP = 'CP';
     const MEAL_PLAN_CODE_MAP = 'MAP';
     const MEAL_PLAN_CODE_AP = 'AP';
+
+    /**
+     * @return string[]
+     */
+    public function propertyStatuses(): array
+    {
+        return [
+            self::PROPERTY_STATUS_ACTIVE,
+            self::PROPERTY_STATUS_INACTIVE,
+            self::PROPERTY_STATUS_BLOCKED,
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function propertyTypes(): array
+    {
+        return [
+            self::PROPERTY_TYPE_HOTEL,
+            self::PROPERTY_TYPE_RESORT,
+            self::PROPERTY_TYPE_HOMESTAY,
+            self::PROPERTY_TYPE_VILLA,
+            self::PROPERTY_TYPE_APARTMENT,
+            self::PROPERTY_TYPE_GUEST_HOUSE,
+            self::PROPERTY_TYPE_LODGE,
+            self::PROPERTY_TYPE_HOUSEBOAT,
+            self::PROPERTY_TYPE_FARM_HOUSE,
+            self::PROPERTY_TYPE_PALACE,
+            self::PROPERTY_TYPE_MOTEL,
+            self::PROPERTY_TYPE_DHARAMSHALA,
+            self::PROPERTY_TYPE_COTTAGE,
+            self::PROPERTY_TYPE_CAMP,
+            self::PROPERTY_TYPE_POD,
+            self::PROPERTY_TYPE_OTHER,
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function inventoryAccuracies(): array
+    {
+        return [
+            self::INVENTORY_ACCURACY_DAILY,
+            self::INVENTORY_ACCURACY_HOURLY,
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function allowedBookingTypes(): array
+    {
+        return [
+            self::BOOKING_TYPE_DAILY,
+            self::BOOKING_TYPE_HOURLY,
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function spaceStatuses(): array
+    {
+        return [
+            self::SPACE_STATUS_ACTIVE,
+            self::SPACE_STATUS_INACTIVE,
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function productStatuses(): array
+    {
+        return [
+            self::PRODUCT_STATUS_ACTIVE,
+            self::PRODUCT_STATUS_INACTIVE,
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function mealPlanCodes(): array
+    {
+        return [
+            self::MEAL_PLAN_CODE_EP,
+            self::MEAL_PLAN_CODE_CP,
+            self::MEAL_PLAN_CODE_MAP,
+            self::MEAL_PLAN_CODE_AP,
+        ];
+    }
 }
