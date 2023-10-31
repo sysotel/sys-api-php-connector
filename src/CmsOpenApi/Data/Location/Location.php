125204f5819ec6e854190df1dd0206c013e04ae9
Ravish Patel <ravish@sysotel.ai>
2023-10-31 15:00:55 +0530

    update

--- src/CmsOpenApi/Data/Location/Location.php
+++ src/CmsOpenApi/Data/Location/Location.php
@@ -14,9 +14,9 @@ class Location extends Data
     /**
      * @param int $id
      * @param string $name
-     * @param int $categorySlug
      * @param string $type
      * @param string $createdAt
+     * @param string|null $categorySlug
      * @param string|null $code
      * @param array $searchKeywords
      * @param string|null $postalCode
@@ -29,11 +29,11 @@ class Location extends Data
     public function __construct(
         public int                $id,
         public string             $name,
-        public int                $categorySlug,
         public string             $type,
         public string             $createdAt,
-        public ?string            $code = null,
         public array              $searchKeywords = [],
+        public ?string            $categorySlug = null,
+        public ?string            $code = null,
         public ?string            $postalCode = null,
         public ?GeoPoint          $geoPoint = null,
         public ?LocationReference $city = null,
