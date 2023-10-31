e641cf3a679cc613d5f3bae9d754dbd1eeb26976
Ravish Patel <ravish@sysotel.ai>
2023-10-31 15:02:53 +0530

    update

--- src/CmsOpenApi/Data/common/LocationReference.php
+++ src/CmsOpenApi/Data/common/LocationReference.php
@@ -7,10 +7,10 @@ use Spatie\LaravelData\Data;
 class LocationReference extends Data
 {
     public function __construct(
-        public string $id,
-        public string $name,
-        public string $categorySlug,
-        public string $type,
+        public string  $id,
+        public string  $name,
+        public string  $type,
+        public ?string $categorySlug = null,
     )
     {
     }
