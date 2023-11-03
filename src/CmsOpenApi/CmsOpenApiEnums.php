2157c614bb31436d2d901c3d500296cebb110c70
Ravish Patel <ravish@sysotel.ai>
2023-11-03 21:39:51 +0530

    update

--- src/CmsOpenApi/CmsOpenApiEnums.php
+++ src/CmsOpenApi/CmsOpenApiEnums.php
@@ -107,6 +107,186 @@ enum CmsOpenApiEnums
     const PROPERTY_CONTACT_OWNED_BY_FRONT_DESK = 'FRONT_DESK';
     const PROPERTY_CONTACT_OWNED_BY_UNKNOWN = 'UNKNOWN';
 
+    const CURRENCY_AED = 'AED';
+    const CURRENCY_AFN = 'AFN';
+    const CURRENCY_ALL = 'ALL';
+    const CURRENCY_AMD = 'AMD';
+    const CURRENCY_ANG = 'ANG';
+    const CURRENCY_AOA = 'AOA';
+    const CURRENCY_ARS = 'ARS';
+    const CURRENCY_AUD = 'AUD';
+    const CURRENCY_AWG = 'AWG';
+    const CURRENCY_AZN = 'AZN';
+    const CURRENCY_BAM = 'BAM';
+    const CURRENCY_BBD = 'BBD';
+    const CURRENCY_BDT = 'BDT';
+    const CURRENCY_BGN = 'BGN';
+    const CURRENCY_BHD = 'BHD';
+    const CURRENCY_BIF = 'BIF';
+    const CURRENCY_BMD = 'BMD';
+    const CURRENCY_BND = 'BND';
+    const CURRENCY_BOB = 'BOB';
+    const CURRENCY_BOV = 'BOV';
+    const CURRENCY_BRL = 'BRL';
+    const CURRENCY_BSD = 'BSD';
+    const CURRENCY_BTN = 'BTN';
+    const CURRENCY_BWP = 'BWP';
+    const CURRENCY_BYN = 'BYN';
+    const CURRENCY_BZD = 'BZD';
+    const CURRENCY_CAD = 'CAD';
+    const CURRENCY_CDF = 'CDF';
+    const CURRENCY_CHE = 'CHE';
+    const CURRENCY_CHF = 'CHF';
+    const CURRENCY_CHW = 'CHW';
+    const CURRENCY_CLF = 'CLF';
+    const CURRENCY_CLP = 'CLP';
+    const CURRENCY_COP = 'COP';
+    const CURRENCY_COU = 'COU';
+    const CURRENCY_CRC = 'CRC';
+    const CURRENCY_CUP = 'CUP';
+    const CURRENCY_CVE = 'CVE';
+    const CURRENCY_CZK = 'CZK';
+    const CURRENCY_DJF = 'DJF';
+    const CURRENCY_DKK = 'DKK';
+    const CURRENCY_DOP = 'DOP';
+    const CURRENCY_DZD = 'DZD';
+    const CURRENCY_EGP = 'EGP';
+    const CURRENCY_ERN = 'ERN';
+    const CURRENCY_ETB = 'ETB';
+    const CURRENCY_EUR = 'EUR';
+    const CURRENCY_FJD = 'FJD';
+    const CURRENCY_FKP = 'FKP';
+    const CURRENCY_GBP = 'GBP';
+    const CURRENCY_GEL = 'GEL';
+    const CURRENCY_GHS = 'GHS';
+    const CURRENCY_GIP = 'GIP';
+    const CURRENCY_GMD = 'GMD';
+    const CURRENCY_GNF = 'GNF';
+    const CURRENCY_GTQ = 'GTQ';
+    const CURRENCY_GYD = 'GYD';
+    const CURRENCY_HKD = 'HKD';
+    const CURRENCY_HNL = 'HNL';
+    const CURRENCY_HTG = 'HTG';
+    const CURRENCY_HUF = 'HUF';
+    const CURRENCY_IDR = 'IDR';
+    const CURRENCY_ILS = 'ILS';
+    const CURRENCY_INR = 'INR';
+    const CURRENCY_IQD = 'IQD';
+    const CURRENCY_IRR = 'IRR';
+    const CURRENCY_ISK = 'ISK';
+    const CURRENCY_JMD = 'JMD';
+    const CURRENCY_JOD = 'JOD';
+    const CURRENCY_JPY = 'JPY';
+    const CURRENCY_KES = 'KES';
+    const CURRENCY_KGS = 'KGS';
+    const CURRENCY_KHR = 'KHR';
+    const CURRENCY_KMF = 'KMF';
+    const CURRENCY_KPW = 'KPW';
+    const CURRENCY_KRW = 'KRW';
+    const CURRENCY_KWD = 'KWD';
+    const CURRENCY_KYD = 'KYD';
+    const CURRENCY_KZT = 'KZT';
+    const CURRENCY_LAK = 'LAK';
+    const CURRENCY_LBP = 'LBP';
+    const CURRENCY_LKR = 'LKR';
+    const CURRENCY_LRD = 'LRD';
+    const CURRENCY_LSL = 'LSL';
+    const CURRENCY_LYD = 'LYD';
+    const CURRENCY_MAD = 'MAD';
+    const CURRENCY_MDL = 'MDL';
+    const CURRENCY_MGA = 'MGA';
+    const CURRENCY_MKD = 'MKD';
+    const CURRENCY_MMK = 'MMK';
+    const CURRENCY_MNT = 'MNT';
+    const CURRENCY_MOP = 'MOP';
+    const CURRENCY_MRU = 'MRU';
+    const CURRENCY_MUR = 'MUR';
+    const CURRENCY_MVR = 'MVR';
+    const CURRENCY_MWK = 'MWK';
+    const CURRENCY_MXN = 'MXN';
+    const CURRENCY_MXV = 'MXV';
+    const CURRENCY_MYR = 'MYR';
+    const CURRENCY_MZN = 'MZN';
+    const CURRENCY_NAD = 'NAD';
+    const CURRENCY_NGN = 'NGN';
+    const CURRENCY_NIO = 'NIO';
+    const CURRENCY_NOK = 'NOK';
+    const CURRENCY_NPR = 'NPR';
+    const CURRENCY_NZD = 'NZD';
+    const CURRENCY_OMR = 'OMR';
+    const CURRENCY_PAB = 'PAB';
+    const CURRENCY_PEN = 'PEN';
+    const CURRENCY_PGK = 'PGK';
+    const CURRENCY_PHP = 'PHP';
+    const CURRENCY_PKR = 'PKR';
+    const CURRENCY_PLN = 'PLN';
+    const CURRENCY_PYG = 'PYG';
+    const CURRENCY_QAR = 'QAR';
+    const CURRENCY_RON = 'RON';
+    const CURRENCY_RSD = 'RSD';
+    const CURRENCY_CNY = 'CNY';
+    const CURRENCY_RUB = 'RUB';
+    const CURRENCY_RWF = 'RWF';
+    const CURRENCY_SAR = 'SAR';
+    const CURRENCY_SBD = 'SBD';
+    const CURRENCY_SCR = 'SCR';
+    const CURRENCY_SDG = 'SDG';
+    const CURRENCY_SEK = 'SEK';
+    const CURRENCY_SGD = 'SGD';
+    const CURRENCY_SHP = 'SHP';
+    const CURRENCY_SLE = 'SLE';
+    const CURRENCY_SLL = 'SLL';
+    const CURRENCY_SOS = 'SOS';
+    const CURRENCY_SRD = 'SRD';
+    const CURRENCY_SSP = 'SSP';
+    const CURRENCY_STN = 'STN';
+    const CURRENCY_SVC = 'SVC';
+    const CURRENCY_SYP = 'SYP';
+    const CURRENCY_SZL = 'SZL';
+    const CURRENCY_THB = 'THB';
+    const CURRENCY_TJS = 'TJS';
+    const CURRENCY_TMT = 'TMT';
+    const CURRENCY_TND = 'TND';
+    const CURRENCY_TOP = 'TOP';
+    const CURRENCY_TRY = 'TRY';
+    const CURRENCY_TTD = 'TTD';
+    const CURRENCY_TWD = 'TWD';
+    const CURRENCY_TZS = 'TZS';
+    const CURRENCY_UAH = 'UAH';
+    const CURRENCY_UGX = 'UGX';
+    const CURRENCY_USD = 'USD';
+    const CURRENCY_USN = 'USN';
+    const CURRENCY_UYI = 'UYI';
+    const CURRENCY_UYU = 'UYU';
+    const CURRENCY_UYW = 'UYW';
+    const CURRENCY_UZS = 'UZS';
+    const CURRENCY_VED = 'VED';
+    const CURRENCY_VES = 'VES';
+    const CURRENCY_VND = 'VND';
+    const CURRENCY_VUV = 'VUV';
+    const CURRENCY_WST = 'WST';
+    const CURRENCY_XAF = 'XAF';
+    const CURRENCY_XAG = 'XAG';
+    const CURRENCY_XAU = 'XAU';
+    const CURRENCY_XBA = 'XBA';
+    const CURRENCY_XBB = 'XBB';
+    const CURRENCY_XBC = 'XBC';
+    const CURRENCY_XBD = 'XBD';
+    const CURRENCY_XCD = 'XCD';
+    const CURRENCY_XDR = 'XDR';
+    const CURRENCY_XOF = 'XOF';
+    const CURRENCY_XPD = 'XPD';
+    const CURRENCY_XPF = 'XPF';
+    const CURRENCY_XPT = 'XPT';
+    const CURRENCY_XSU = 'XSU';
+    const CURRENCY_XTS = 'XTS';
+    const CURRENCY_XUA = 'XUA';
+    const CURRENCY_XXX = 'XXX';
+    const CURRENCY_YER = 'YER';
+    const CURRENCY_ZAR = 'ZAR';
+    const CURRENCY_ZMW = 'ZMW';
+    const CURRENCY_ZWL = 'ZWL';
+
     /**
      * @return string[]
      */
@@ -277,6 +457,194 @@ enum CmsOpenApiEnums
         return [
             self::CHANNEL_ID_SYSOTEL_CMS_OPEN_API,
             self::CHANNEL_ID_AGGREGATE_INTELLIGENCE,
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function currencies(): array
+    {
+        return [
+            self::CURRENCY_AED,
+            self::CURRENCY_AFN,
+            self::CURRENCY_ALL,
+            self::CURRENCY_AMD,
+            self::CURRENCY_ANG,
+            self::CURRENCY_AOA,
+            self::CURRENCY_ARS,
+            self::CURRENCY_AUD,
+            self::CURRENCY_AWG,
+            self::CURRENCY_AZN,
+            self::CURRENCY_BAM,
+            self::CURRENCY_BBD,
+            self::CURRENCY_BDT,
+            self::CURRENCY_BGN,
+            self::CURRENCY_BHD,
+            self::CURRENCY_BIF,
+            self::CURRENCY_BMD,
+            self::CURRENCY_BND,
+            self::CURRENCY_BOB,
+            self::CURRENCY_BOV,
+            self::CURRENCY_BRL,
+            self::CURRENCY_BSD,
+            self::CURRENCY_BTN,
+            self::CURRENCY_BWP,
+            self::CURRENCY_BYN,
+            self::CURRENCY_BZD,
+            self::CURRENCY_CAD,
+            self::CURRENCY_CDF,
+            self::CURRENCY_CHE,
+            self::CURRENCY_CHF,
+            self::CURRENCY_CHW,
+            self::CURRENCY_CLF,
+            self::CURRENCY_CLP,
+            self::CURRENCY_COP,
+            self::CURRENCY_COU,
+            self::CURRENCY_CRC,
+            self::CURRENCY_CUP,
+            self::CURRENCY_CVE,
+            self::CURRENCY_CZK,
+            self::CURRENCY_DJF,
+            self::CURRENCY_DKK,
+            self::CURRENCY_DOP,
+            self::CURRENCY_DZD,
+            self::CURRENCY_EGP,
+            self::CURRENCY_ERN,
+            self::CURRENCY_ETB,
+            self::CURRENCY_EUR,
+            self::CURRENCY_FJD,
+            self::CURRENCY_FKP,
+            self::CURRENCY_GBP,
+            self::CURRENCY_GEL,
+            self::CURRENCY_GHS,
+            self::CURRENCY_GIP,
+            self::CURRENCY_GMD,
+            self::CURRENCY_GNF,
+            self::CURRENCY_GTQ,
+            self::CURRENCY_GYD,
+            self::CURRENCY_HKD,
+            self::CURRENCY_HNL,
+            self::CURRENCY_HTG,
+            self::CURRENCY_HUF,
+            self::CURRENCY_IDR,
+            self::CURRENCY_ILS,
+            self::CURRENCY_INR,
+            self::CURRENCY_IQD,
+            self::CURRENCY_IRR,
+            self::CURRENCY_ISK,
+            self::CURRENCY_JMD,
+            self::CURRENCY_JOD,
+            self::CURRENCY_JPY,
+            self::CURRENCY_KES,
+            self::CURRENCY_KGS,
+            self::CURRENCY_KHR,
+            self::CURRENCY_KMF,
+            self::CURRENCY_KPW,
+            self::CURRENCY_KRW,
+            self::CURRENCY_KWD,
+            self::CURRENCY_KYD,
+            self::CURRENCY_KZT,
+            self::CURRENCY_LAK,
+            self::CURRENCY_LBP,
+            self::CURRENCY_LKR,
+            self::CURRENCY_LRD,
+            self::CURRENCY_LSL,
+            self::CURRENCY_LYD,
+            self::CURRENCY_MAD,
+            self::CURRENCY_MDL,
+            self::CURRENCY_MGA,
+            self::CURRENCY_MKD,
+            self::CURRENCY_MMK,
+            self::CURRENCY_MNT,
+            self::CURRENCY_MOP,
+            self::CURRENCY_MRU,
+            self::CURRENCY_MUR,
+            self::CURRENCY_MVR,
+            self::CURRENCY_MWK,
+            self::CURRENCY_MXN,
+            self::CURRENCY_MXV,
+            self::CURRENCY_MYR,
+            self::CURRENCY_MZN,
+            self::CURRENCY_NAD,
+            self::CURRENCY_NGN,
+            self::CURRENCY_NIO,
+            self::CURRENCY_NOK,
+            self::CURRENCY_NPR,
+            self::CURRENCY_NZD,
+            self::CURRENCY_OMR,
+            self::CURRENCY_PAB,
+            self::CURRENCY_PEN,
+            self::CURRENCY_PGK,
+            self::CURRENCY_PHP,
+            self::CURRENCY_PKR,
+            self::CURRENCY_PLN,
+            self::CURRENCY_PYG,
+            self::CURRENCY_QAR,
+            self::CURRENCY_RON,
+            self::CURRENCY_RSD,
+            self::CURRENCY_CNY,
+            self::CURRENCY_RUB,
+            self::CURRENCY_RWF,
+            self::CURRENCY_SAR,
+            self::CURRENCY_SBD,
+            self::CURRENCY_SCR,
+            self::CURRENCY_SDG,
+            self::CURRENCY_SEK,
+            self::CURRENCY_SGD,
+            self::CURRENCY_SHP,
+            self::CURRENCY_SLE,
+            self::CURRENCY_SLL,
+            self::CURRENCY_SOS,
+            self::CURRENCY_SRD,
+            self::CURRENCY_SSP,
+            self::CURRENCY_STN,
+            self::CURRENCY_SVC,
+            self::CURRENCY_SYP,
+            self::CURRENCY_SZL,
+            self::CURRENCY_THB,
+            self::CURRENCY_TJS,
+            self::CURRENCY_TMT,
+            self::CURRENCY_TND,
+            self::CURRENCY_TOP,
+            self::CURRENCY_TRY,
+            self::CURRENCY_TTD,
+            self::CURRENCY_TWD,
+            self::CURRENCY_TZS,
+            self::CURRENCY_UAH,
+            self::CURRENCY_UGX,
+            self::CURRENCY_USD,
+            self::CURRENCY_USN,
+            self::CURRENCY_UYI,
+            self::CURRENCY_UYU,
+            self::CURRENCY_UYW,
+            self::CURRENCY_UZS,
+            self::CURRENCY_VED,
+            self::CURRENCY_VES,
+            self::CURRENCY_VND,
+            self::CURRENCY_VUV,
+            self::CURRENCY_WST,
+            self::CURRENCY_XAF,
+            self::CURRENCY_XAG,
+            self::CURRENCY_XAU,
+            self::CURRENCY_XBA,
+            self::CURRENCY_XBB,
+            self::CURRENCY_XBC,
+            self::CURRENCY_XBD,
+            self::CURRENCY_XCD,
+            self::CURRENCY_XDR,
+            self::CURRENCY_XOF,
+            self::CURRENCY_XPD,
+            self::CURRENCY_XPF,
+            self::CURRENCY_XPT,
+            self::CURRENCY_XSU,
+            self::CURRENCY_XTS,
+            self::CURRENCY_XUA,
+            self::CURRENCY_XXX,
+            self::CURRENCY_YER,
+            self::CURRENCY_ZAR,
+            self::CURRENCY_ZMW,
+            self::CURRENCY_ZWL,
         ];
     }
 }
