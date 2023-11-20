fdfbbe31b94a80c1de5f9683b2029207d7295b16
Ravish Patel <ravish@sysotel.ai>
2023-11-20 11:14:50 +0530

    upat

--- config/sys-api-connector.php
+++ config/sys-api-connector.php
@@ -5,7 +5,7 @@ return [
         'base_url' => env('SYSOTEL_CMS_OPEN_API_URL'),
         'client_id' => env('SYSOTEL_CMS_OPEN_API_CLIENT_ID'),
         'api_token' => env('SYSOTEL_CMS_OPEN_API_TOKEN'),
-        'date_format' => 'd-M-Y',
+        'date_format' => 'Y-m-d',
         'timestamp_format' => 'Y-m-d\TH:i:sP',
     ],
 ];
