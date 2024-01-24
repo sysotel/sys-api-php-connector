0fb97a82dc1412fb0ad9ea2d94e4e82e983a391e
Ravish Patel <ravish@sysotel.ai>
2024-01-24 16:25:20 +0530

    update

--- src/CmsOpenApi/Data/PropertyPolicy/common/AgePolicy.php
+++ src/CmsOpenApi/Data/PropertyPolicy/common/AgePolicy.php
@@ -7,11 +7,11 @@ use Spatie\LaravelData\Data;
 class AgePolicy extends Data
 {
     /**
-     * @param int|null $adultAgeDescription
      * @param int|null $infantAgeThreshold
      * @param int|null $childAgeThreshold
      * @param int|null $freeChildAgeThreshold
      * @param int|null $noOfFreeChildGranted
+     * @param string|null $adultAgeDescription
      * @param string|null $infantAgeThresholdDescription
      * @param string|null $childAgeThresholdDescription
      * @param string|null $freeChildAgeThresholdDescription
@@ -19,11 +19,11 @@ class AgePolicy extends Data
      * @param array|null $description
      */
     public function __construct(
-        public int|null $adultAgeDescription = null,
         public int|null $infantAgeThreshold = null,
         public int|null $childAgeThreshold = null,
         public int|null $freeChildAgeThreshold = null,
         public int|null $noOfFreeChildGranted = null,
+        public string|null $adultAgeDescription = null,
         public string|null $infantAgeThresholdDescription = null,
         public string|null $childAgeThresholdDescription = null,
         public string|null $freeChildAgeThresholdDescription = null,
