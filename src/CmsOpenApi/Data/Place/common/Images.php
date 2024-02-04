0361e63f202aebb5466a01603fe1cd9a0ae9535b
Ravish Patel <ravish@sysotel.ai>
2024-02-04 12:05:58 +0530

    update

--- src/CmsOpenApi/Data/Place/common/Images.php
+++ src/CmsOpenApi/Data/Place/common/Images.php
@@ -2,47 +2,25 @@
 
 namespace SYSOTEL\APP\ApiConnector\CmsOpenApi\Data\Place\common;
 
-use Spatie\LaravelData\Attributes\DataCollectionOf;
 use Spatie\LaravelData\Data;
-use Spatie\LaravelData\DataCollection;
-use SYSOTEL\APP\ApiConnector\CmsOpenApi\Data\Location\common\LocationChannel;
+use SYSOTEL\APP\ApiConnector\CmsOpenApi\Data\common\ImageUrls;
 
 class Images extends Data
 {
     /**
-     * @param string|null $_id
+     * @param string|null $id
      * @param string|null $source
      * @param string|null $sourceUrl
-     * @param bool|null $isFeatured
-     * @param PlaceType|null $type
-     * @param int|null $rating
-     * @param string|null $text
-     * @param bool|null $translated
-     * @param string|null $timestamp
-     * @param int|null $userRatingsTotal
-     * @param DataCollection|null $items
+     * @param ImageUrls|null $urls
      */
     public function __construct(
-        public ?string         $_id,
-        public ?string         $source,
-        public ?string         $sourceUrl,
-        public ?bool           $isFeatured,
-        public ?PlaceType      $type,
-        public ?int            $rating,
-        public ?string         $text,
-        public ?bool           $translated,
-        public ?string         $timestamp,
-        public ?int            $userRatingsTotal,
-
-
-        #[DataCollectionOf(Items::class)]
-        public ?DataCollection $items = null,
-
+        public ?string    $id,
+        public ?string    $source = null,
+        public ?string    $sourceUrl = null,
+        public ?ImageUrls $urls = null,
     )
     {
     }
-
-
 }
 
 
